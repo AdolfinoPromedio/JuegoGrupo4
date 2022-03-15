0ff4f0d268f4c87ef7b508103ee560151376ffa4
AdolfinoPromedio <100725770+AdolfinoPromedio@users.noreply.github.com>
2022-03-15 00:15:36 -0300

    Update puente.c

--- puente.c
+++ puente.c
@@ -1,61 +1,38 @@
-int random1()
-{
-  // Función que devuelve de manera aleatoria un número entero entre 0 y 1.
-  int numero; 
-  numero = random() % 2;
-  return numero;
-}
 
-void ganador(int respuesta, int numero){
-  // Procedimiento que solicita 2 números, uno es la respuesta del jugador (derecha o izquierda) y la otra es el número aleatorio de la función anterior. Compara los números y dependiendo si son iguales o no, imprime un mensaje especifico.
-  if (respuesta == numero){
-    printf("Muy bien, continúa.\n\n");
+void puente(int intento, int lado)
+{
+  // Procedimiento que solicita 2 números, el número de intento del juego, y el lado elegido por el jugador (0 o 1). Se genera el puente, el cual es una matriz de caracteres. Luego mediante un ciclo anidado se imprime la posición del jugador, dependiendo de los parametros dados.
+  
+  char *puente[11][6] = {{"[", " ", "]", "[", " ", "]"},
+  {"[", " ", "]", "[", " ", "]"},
+  {"[", " ", "]", "[", " ", "]"},
+  {"[", " ", "]", "[", " ", "]"},
+  {"[", " ", "]", "[", " ", "]"},
+  {"[", " ", "]", "[", " ", "]"},
+  {"[", " ", "]", "[", " ", "]"},
+  {"[", " ", "]", "[", " ", "]"},
+  {"[", " ", "]", "[", " ", "]"},
+  {"[", " ", "]", "[", " ", "]"},
+  {"[", " ", "]", "[", " ", "]"}};
+
+  int a;
+  if (lado==0){
+    a = 1;
   }
   else {
-    printf("Mala elección, HAS MUERTO.");
-  }
-}
-
-
-
-int main() {
-
-  Menu();
-
-  // Solicita el nombre del jugador
-  char nombre[30];
-  printf("Ingresa tu nombre (solo una palabra): ");
-  scanf("%s", nombre);
-  
-  bienvenida(nombre);
-
-  int k;
-  int n;
-  n = 0;
-  for (k=1; k < 12; k++) {
-    puente(k,n);
-    srand(time(NULL));
-  
-    int numero_random;
-    numero_random = random1();
-    //printf("%d", number);
-
-    if (k == 11){
-      victoria();
-      break;
-    }
-    
-    int opcion_usuario;
-    printf("Ingresa una opción: ");
-    scanf("%d", &opcion_usuario);
-    
-    ganador(opcion_usuario, numero_random);
-    n = opcion_usuario;
-    if (opcion_usuario!=numero_random){
-      break;
-    }
-    
+    a = 4;
   }
   
-  return 0;
+  int i;
+  int j;
+  
+  for (i = 0; i < 11; i++) {
+    for (j = 0; j < 6; j++){
+      puente[11-intento][a] = "O";
+      printf("%s", puente[i][j]);
+    }
+    
+    printf("\n");
+  };
+  printf("\n");
 }
